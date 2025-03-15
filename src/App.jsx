a3fda525994cb111ccbb72a03b2702c5a0c2f40d
jamleads <ogunyemiadetunji17@gmail.com>
2025-03-15 20:14:41 +0100

    banck statement price

--- src/App.jsx
+++ src/App.jsx
@@ -514,7 +514,7 @@ function App() {
                       data-aos="fade-down-right"
                     >
                       <div className="w-2/5 h-[120px] mb-5 bg-theDarkGold the-path rounded-l-3xl flex flex-col items-center justify-center text-white">
-                        <p className="text-xl">$75 Each</p>
+                        <p className="text-xl">$85 Each</p>
                       </div>
                       <div className="w-3/5 h-full flex items-center justify-center text-base text-white">
                         Bank Statement
